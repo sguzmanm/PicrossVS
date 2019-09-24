d9c6f03a6dc5ad784bc2d140749e751028750eb0
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-09-24 15:52:41 -0500

    Tutorial finished

--- imports/ui/pages/tutorial/Tutorial.jsx
+++ imports/ui/pages/tutorial/Tutorial.jsx
@@ -4,9 +4,21 @@ import Board from '../../components/board/Board.jsx'
 import boards from '../../../util/boards.json'
 
 const Tutorial = () => {
-  const [board1, setBoard1] = useState([...boards[0].curCells])
-  const [board2, setBoard2] = useState([...boards[1].curCells])
-  const [board3, setBoard3] = useState([...boards[2].curCells])
+  const originalBoards = [
+    boards[0].curCells.map(function(arr) {
+      return arr.slice()
+    }),
+    boards[1].curCells.map(function(arr) {
+      return arr.slice()
+    }),
+    boards[2].curCells.map(function(arr) {
+      return arr.slice()
+    }),
+  ]
+
+  const [board1, setBoard1] = useState([...originalBoards[0]])
+  const [board2, setBoard2] = useState([...originalBoards[1]])
+  const [board3, setBoard3] = useState([...originalBoards[2]])
 
   const uncoverCell = (i, j, changeToState, board) => {
     let boardTemp = null
@@ -35,18 +47,17 @@ const Tutorial = () => {
     switch (board) {
       case 1:
         setBoard = setBoard1
-        originalBoard = boards[0].curCells
+        originalBoard = [...originalBoards[0]]
         break
       case 2:
         setBoard = setBoard2
-        originalBoard = boards[1].curCells
+        originalBoard = [...originalBoards[1]]
         break
       case 3:
         setBoard = setBoard3
-        originalBoard = boards[2].curCells
+        originalBoard = [...originalBoards[2]]
         break
     }
-    console.log(originalBoard)
     setBoard(originalBoard)
   }
 
@@ -110,6 +121,47 @@ const Tutorial = () => {
         Reset board
       </button>
 
+      <h2>Second board: Chicken</h2>
+      <ol className="tutorial__list">
+        <li className="tutorial__listItem">
+          No rows or columns with a 5, lets try a different strategy: You see
+          that if we count spaces between numbers as a 1, the column with a 1-3
+          will sum up to 5 (1+1+3=5)
+        </li>
+        <li className="tutorial__listItem">
+          What we can do now is check the first cell in that column, leave a
+          space, and check the last 3. Finally, flag the space with a right
+          click so that you don't try to check it by mistake
+        </li>
+        <li className="tutorial__listItem">
+          From here, there are multiple things that you can make: for example,
+          you see that besides the flag space there are 2 groups with 2
+          consecutive cells each, just as the row says!. You can check those
+        </li>
+        <li className="tutorial__listItem">
+          Another posibility is that you flag the spaces at the last row.
+          Because the row says there is only one cell that needs to be checked,
+          we know there are no more cells to check in this row. Therefore, you
+          can flag the rest of the row. This is the same for the last column
+        </li>
+        <li className="tutorial__listItem">
+          You could also check the cells in the middle the 3rd and 4th row. Why?
+          Because these rows require 4 consecutive cells. This means that no
+          matter how you try, the cells in the middle will always need to be
+          checked
+        </li>
+        <li className="tutorial__listItem">
+          Now that you have done those steps, you can see that the last free
+          cells (the one that are not flagged) in the first column can be
+          checked. Because that way we complete the 4 that is needed. Note that
+          the same is true for the first row
+        </li>
+        <li className="tutorial__listItem">
+          You can reset the board with the button below and try to solve it in
+          different ways, practice makes perfect, specially in Picross
+        </li>
+      </ol>
+
       <Board
         board={boards[1]}
         uncoverCell={(i, j, correct) => uncoverCell(i, j, correct, 2)}
@@ -119,6 +171,30 @@ const Tutorial = () => {
       <button className="tutorial__button" onClick={() => resetBoard(2)}>
         Reset board
       </button>
+
+      <h2>Third board: Butterfly</h2>
+      <ol className="tutorial__list">
+        <li className="tutorial__listItem">
+          This board is way bigger now! And yes, we will leave it all to you.
+          But we will also give you some final tips
+        </li>
+        <li className="tutorial__listItem">
+          Try to find the biggest number in the rows or columns, if the number
+          is bigger than the half of the size of the board, then you know that
+          at least the cells in the middle need to be checked
+        </li>
+        <li className="tutorial__listItem">
+          The previous statement is also true for rows or columns which have
+          many numbers spread. For example a hint with 2-3-3 is as valuable as a
+          hint with a 10 (both add up to 10 if you count the spaces between
+          numbers as 1)
+        </li>
+        <li className="tutorial__listItem">
+          Don't forget to flag (Right click): Flagging prevents you from making
+          mistakes and also leave less space to check other rows or columns,
+          this will make easier finding where to check the next cell
+        </li>
+      </ol>
 
       <Board
         board={boards[2]}
