c3b509ce28c9be0402bd15493b7f345de90848a6
korkies22 <cm.sarmiento10@uniandes.edu.co>
2019-09-24 17:57:18 -0500

    Arreglado diseño de login

--- imports/ui/app/Router.jsx
+++ imports/ui/app/Router.jsx
@@ -1,46 +1,54 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch
-} from "react-router-dom";
-import { PropTypes } from "prop-types";
+  Switch,
+} from 'react-router-dom';
+import { PropTypes } from 'prop-types';
 
-import Tutorial from "../pages/tutorial/Tutorial.jsx";
-import Game from "../pages/game/Game.jsx";
-import Hub from "../pages/hub/Hub.jsx";
+import './Router.scss';
+
+import Tutorial from '../pages/tutorial/Tutorial.jsx';
+import Game from '../pages/game/Game.jsx';
+import Hub from '../pages/hub/Hub.jsx';
 
 function AppRouter(props) {
   const userId = props.userId;
   const currentUser = props.currentUser;
   return (
-    <Router>
-      <Switch>
-        <Route
-          path='/'
-          exact
-          render={() => (userId ? <Redirect to='/hub' /> : <Tutorial />)}
-        />
-        <Route
-          path='/hub'
-          render={props => (userId ? <Hub {...props} /> : <Redirect to='/' />)}
-        />
-        <Route
-          path='/game/:id'
-          render={props =>
-            userId ? (
-              <Game {...props} currentUser={currentUser} />
-            ) : (
-              <Redirect to='/' />
-            )
-          }
-        />
-        <Route
-          render={() => (userId ? <Redirect to='/hub' /> : <Redirect to='/' />)}
-        />
-      </Switch>
-    </Router>
+    <div className="router">
+      <Router>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => (userId ? <Redirect to="/hub" /> : <Tutorial />)}
+          />
+          <Route
+            path="/hub"
+            render={props =>
+              userId ? <Hub {...props} /> : <Redirect to="/" />
+            }
+          />
+          <Route
+            path="/game/:id"
+            render={props =>
+              userId ? (
+                <Game {...props} currentUser={currentUser} />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
+          <Route
+            render={() =>
+              userId ? <Redirect to="/hub" /> : <Redirect to="/" />
+            }
+          />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
@@ -48,5 +56,5 @@ export default AppRouter;
 
 AppRouter.propTypes = {
   userId: PropTypes.string,
-  currentUser: PropTypes.object
+  currentUser: PropTypes.object,
 };
--- imports/ui/components/board/Board.jsx
+++ imports/ui/components/board/Board.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import "./Board.scss";
-import PropTypes from "prop-types";
-import CellHint from "./cellHint/CellHint.jsx";
-import CellGrid from "./cellGrid/CellGrid.jsx";
+import React from 'react';
+import './Board.scss';
+import PropTypes from 'prop-types';
+import CellHint from './cellHint/CellHint.jsx';
+import CellGrid from './cellGrid/CellGrid.jsx';
 const Board = props => {
   let boardStyle = {
     gridTemplateColumns: `repeat(${props.board.columns.length + 1},3rem)`,
-    gridTemplateRows: `4rem repeat(${props.board.rows.length},3rem)`
+    gridTemplateRows: `4rem repeat(${props.board.rows.length},3rem)`,
   };
   return (
-    <div className='board__container'>
-      <h2 className='board__name'>{props.board.name}</h2>
-      <div className='board' style={boardStyle}>
+    <div className="board__container">
+      <h2 className="board__name">{props.board.name}</h2>
+      <div className="board" style={boardStyle}>
         <div></div>
         {props.board.columns.map((row, i) => {
           return (
@@ -19,7 +19,8 @@ const Board = props => {
               i={i}
               hint={props.board.columns[i]}
               key={i}
-              isCol={true}></CellHint>
+              isCol={true}
+            ></CellHint>
           );
         })}
         {props.board.rows.map((row, i) => {
@@ -28,7 +29,8 @@ const Board = props => {
               i={i}
               hint={props.board.rows[i]}
               key={i}
-              isCol={false}></CellHint>
+              isCol={false}
+            ></CellHint>
           );
         })}
         <CellGrid
@@ -47,11 +49,11 @@ Board.propTypes = {
     name: PropTypes.string.isRequired,
     rows: PropTypes.arrayOf(PropTypes.string).isRequired,
     columns: PropTypes.arrayOf(PropTypes.string).isRequired,
-    goal: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired
+    goal: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
   }).isRequired,
   uncoverCell: PropTypes.func.isRequired,
   curCells: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
-  state:PropTypes.number.isRequired
-}
+  state: PropTypes.number.isRequired,
+};
 
 export default Board;
