1c1e308f4a363f129cce477a71125d7ffc8e39f4
Daniel Ramirez <37638663+Dard9811@users.noreply.github.com>
2019-10-14 12:50:31 -0500

    Code review Daniel Ramirez
    Me parece que trabajaron de una manera muy ordena y clara desde un principio, la aplicacion me parece genial y muy facil de usar, creo que cumplierón 
    los objetivos del proyecto y lo hicieron muy bien. Me parecio genial que pudierna desplegar la aplicación ya que no es facil en su totalidad.
    Finalmente quisiera decir que toda la estructura del proyecto es espectacular, muy claro de entender para cualquier persona que este interesado en 
    ver su codigó, los felicito una vez mas. Lo unico que les resaltaria es que comentaran el codigó, para que la gente menos experta pueda tambien entender
    su codigó.

--- imports/ui/app/App.jsx
+++ imports/ui/app/App.jsx
@@ -1,3 +1,9 @@
+/*Me parece que trabajaron de una manera muy ordena y clara desde un principio, la aplicacion me parece genial y muy facil de usar, creo que cumplierón 
+los objetivos del proyecto y lo hicieron muy bien. Me parecio genial que pudierna desplegar la aplicación ya que no es facil en su totalidad.
+Finalmente quisiera decir que toda la estructura del proyecto es espectacular, muy claro de entender para cualquier persona que este interesado en 
+ver su codigó, los felicito una vez mas. Lo unico que les resaltaria es que comentaran el codigó, para que la gente menos experta pueda tambien entender
+su codigó.*/
+
 import React from "react";
 import { Meteor } from "meteor/meteor";
 import { withTracker } from "meteor/react-meteor-data";
